12e364adf9f2a51db7d2bc06d97dcbd2f7c8d6e7
Joani COUMA <joani.couma@gmail.com>
2014-01-23 20:34:41 +0100

    Starting on walking into a bar

--- public/index.js
+++ public/index.js
@@ -7,18 +7,40 @@ var characters = [
 	"Elf"
 ];
 
-window.onload = function () {
-	var ul = document.createElement('ul');
+// PAGE INITIALISATION
+// Every checkboxes is associated with a character
+// Bartender is setup
 
-	document.getElementById('inner-page').appendChild(ul);
+window.onload = function () {
+	//var ul = document.createElement('ul');
+	var form = document.createElement('form');
+	form.setAttribute('action', " ");
+
+	document.getElementById('inner-page').appendChild(form);
 
 	for(var chara in characters)
 	{
-		var li = document.createElement('li');
-		var text = document.createTextNode(characters[chara]);
+		//var li = document.createElement('li');
 
-		li.appendChild(text);
-		ul.appendChild(li);
+		var label = document.createElement('label');
+		var input = document.createElement('input');
+		input.setAttribute('type', 'checkbox');
+		input.setAttribute('name', 'character');
+		input.setAttribute('value', characters[chara]);
+
+		var text = document.
+					createTextNode(characters[chara]);
+
+		label.appendChild(input);
+		label.appendChild(text);
+		form.appendChild(label);
+		//ul.appendChild(li);
 	}
+
+
+	var bartender = document.createElement('h1');
+	var text_bart = document.createTextNode('Bartender is bored');
+	bartender.appendChild(text_bart);
+	document.getElementById('headline').appendChild(bartender);
 
 }
