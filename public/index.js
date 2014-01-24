dc803786168d9e9048481a85247d3b94c60521c1
Joani COUMA <joani.couma@gmail.com>
2014-01-24 09:12:08 +0100

    Walk into a bar completed

--- public/index.js
+++ public/index.js
@@ -15,6 +15,7 @@ window.onload = function () {
 	//var ul = document.createElement('ul');
 	var form = document.createElement('form');
 	form.setAttribute('action', " ");
+	form.setAttribute('id', 'bar-form');
 
 	document.getElementById('inner-page').appendChild(form);
 
@@ -37,10 +38,61 @@ window.onload = function () {
 		//ul.appendChild(li);
 	}
 
+	form.addEventListener('click', checkboxOnClick, false);
+
 
 	var bartender = document.createElement('h1');
+	bartender.setAttribute('id', 'bartender');
 	var text_bart = document.createTextNode('Bartender is bored');
 	bartender.appendChild(text_bart);
 	document.getElementById('headline').appendChild(bartender);
+
+}
+
+/**
+*	Called when the user click on a checkbox
+*/
+function checkboxOnClick(e) {
+	console.log('click');
+
+	var elem_value = e.target.getAttribute('value');
+
+	walksIntoABar(elem_value);
+}
+
+/**
+*	Modifying the headline with the characters clicked
+*/
+function walksIntoABar(string) {
+
+	var active_chars = [];
+	var results;
+	var bar = document.getElementById('bar-form');
+
+	var chars = bar.elements;
+
+	for(var i = 0; i < chars.length; i++) {
+		if(chars[i].type == 'checkbox') {
+			if(chars[i].checked == true) {
+				//console.log(chars[i].value);
+				active_chars.push(chars[i].value);
+			}
+		}
+	}
+	var bart_head = document.getElementById('bartender');
+	var string_head;
+
+	if(active_chars.length == 0) {
+		string_head = 'Bartender is bored';
+	}
+	else if(active_chars.length == 1) {
+		string_head = active_chars[0] + " walks into a bar.";
+	}
+	else {
+		string_head = active_chars.join(', ') + " walk into a bar.";
+	}
+
+	bart_head.innerHTML = string_head;
+
 
 }
